f4cc1285f6a8c76234ae5db59535b42c0f151d9f
smiggleworth <jrepanich@gmail.com>
2025-01-06 10:50:16 -0500

    args are ref types and now protos

--- pkg/stores/azure_store.go
+++ pkg/stores/azure_store.go
@@ -23,56 +23,56 @@ type AzureStreamStore struct {
 }
 
 // CreateTier implements StreamStore.
-func (s *AzureStreamStore) CreateTier(ctx context.Context, args models.CreateTierArgs) error {
+func (s *AzureStreamStore) CreateTier(ctx context.Context, args *models.CreateTierArgs) error {
 	panic("unimplemented")
 }
 
 // DeletePage implements StreamStore.
-func (s *AzureStreamStore) DeletePage(ctx context.Context, args models.DeletePageArgs) error {
+func (s *AzureStreamStore) DeletePage(ctx context.Context, args *models.DeletePageArgs) error {
 	panic("unimplemented")
 }
 
 // DeletePartition implements StreamStore.
-func (s *AzureStreamStore) DeletePartition(ctx context.Context, args models.DeletePartitionArgs) error {
+func (s *AzureStreamStore) DeletePartition(ctx context.Context, args *models.DeletePartitionArgs) error {
 	panic("unimplemented")
 }
 
 // DeleteSpace implements StreamStore.
-func (s *AzureStreamStore) DeleteSpace(ctx context.Context, args models.DeleteSpaceArgs) error {
+func (s *AzureStreamStore) DeleteSpace(ctx context.Context, args *models.DeleteSpaceArgs) error {
 	panic("unimplemented")
 }
 
 // GetPages implements StreamStore.
-func (s *AzureStreamStore) GetPages(ctx context.Context, args models.GetPagesArgs) enumerators.Enumerator[int32] {
+func (s *AzureStreamStore) GetPages(ctx context.Context, args *models.GetPagesArgs) enumerators.Enumerator[int32] {
 	panic("unimplemented")
 }
 
 // GetPartitions implements StreamStore.
-func (s *AzureStreamStore) GetPartitions(ctx context.Context, args models.GetPartitionsArgs) enumerators.Enumerator[string] {
+func (s *AzureStreamStore) GetPartitions(ctx context.Context, args *models.GetPartitionsArgs) enumerators.Enumerator[string] {
 	panic("unimplemented")
 }
 
 // GetSpaces implements StreamStore.
-func (s *AzureStreamStore) GetSpaces(ctx context.Context, args models.GetSpacesArgs) enumerators.Enumerator[string] {
+func (s *AzureStreamStore) GetSpaces(ctx context.Context, args *models.GetSpacesArgs) enumerators.Enumerator[string] {
 	panic("unimplemented")
 }
 
 // ReadManifest implements StreamStore.
-func (s *AzureStreamStore) ReadManifest(ctx context.Context, args models.ReadManifestArgs) (*models.ManifestWrapper, error) {
+func (s *AzureStreamStore) ReadManifest(ctx context.Context, args *models.ReadManifestArgs) (*models.ManifestWrapper, error) {
 	panic("unimplemented")
 }
 
 // ReadPage implements StreamStore.
-func (s *AzureStreamStore) ReadPage(ctx context.Context, args models.ReadPageArgs) enumerators.Enumerator[*models.Entry] {
+func (s *AzureStreamStore) ReadPage(ctx context.Context, args *models.ReadPageArgs) enumerators.Enumerator[*models.Entry] {
 	panic("unimplemented")
 }
 
 // WriteManifest implements StreamStore.
-func (s *AzureStreamStore) WriteManifest(ctx context.Context, args models.WriteManifestArgs) (models.ConcurrencyTag, error) {
+func (s *AzureStreamStore) WriteManifest(ctx context.Context, args *models.WriteManifestArgs) (models.ConcurrencyTag, error) {
 	panic("unimplemented")
 }
 
 // WritePage implements StreamStore.
-func (s *AzureStreamStore) WritePage(ctx context.Context, args models.WritePageArgs) (*models.Page, error) {
+func (s *AzureStreamStore) WritePage(ctx context.Context, args *models.WritePageArgs, entries enumerators.Enumerator[*models.Entry]) (*models.Page, error) {
 	panic("unimplemented")
 }
